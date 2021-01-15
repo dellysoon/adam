a4965b2e4ede850f2ff001bbf192f2d20f908f59
dellysoon <Coledrogers@gmail.com>
2021-01-14 20:29:44 -0500

    Project with Adam (IlearntoCode

---
+++ main.py
@@ -0,0 +1,48 @@
+class Cole():
+    name = "cole"
+    dob = "08/06/2000"
+    Age = 20
+    sex = "all the time"
+    nitin = "number one mother asshole"
+    adam = "cave bird"
+    drink = "Tequila Old Fashioned"
+
+cole = Cole()
+
+class Carly():
+    name = "carly"
+    age = 21
+    ass = True
+    bitch = True
+
+carly = Carly()
+
+
+def print_Cole():
+    print(Cole.name)
+    print(Cole.drink)
+    print(Cole.adam)
+    print(Cole.Age)
+    print(Cole.dob)
+    print(Cole.nitin)
+
+if carly.bitch == False :
+
+
+    print_Cole()
+
+
+else:
+    counter = []
+    counter.__len__()
+
+    for count in counter:
+
+        print("carly is not a bitch, jk")
+
+
+
+
+
+
+
